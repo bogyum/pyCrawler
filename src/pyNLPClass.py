5909d40e95dbe473aed9665d5744b04ac3bf733d
jarvis <bogyum@icloud.com>
2020-06-09 13:31:18 +0900

    Do make NLP class

--- src/pyNLPClass.py
+++ src/pyNLPClass.py
@@ -1,6 +1,11 @@
-import nltk, logging
+import logging
+import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
 
 class NLPAnalyzer:
+
+    def __init__(self):
+        self.lemmatizer = WordNetLemmatizer()
 
     def setNLTKPath(self, path):
         nltk.data.path.append(path)
@@ -16,10 +21,25 @@ class NLPAnalyzer:
 
             sentToken = nltk.sent_tokenize(context, "english")
 
-            # Need to add progress bar
+            # 제목 분석
+            headLineWordTokens = nltk.pos_tag(nltk.word_tokenize(headLine))
+
+            # 본문 문장 단위 분석
             for sentence in sentToken:
                 if str(sentence).find("Updated") > -1 or str(sentence).find("Reporter") > -1:
                     continue
 
-                logging.info("      NLPAnalyzer Class() - sentence :: %s" % sentence)
-                wordTokens = nltk.pos_tag(nltk.word_tokenize(sentence))
+                # tokenizing - pos tagging - lemmatizing - frequency
+                logging.info("NLPAnalyzer - doMorphemeAnalysis() - sentence : %s" % sentence)
+
+                # Tokenizing
+                tokens = nltk.word_tokenize(str(sentence).lower())
+
+                # POS Tagging
+                poses = nltk.pos_tag(tokens)
+
+                # lemmatizing
+                posesList = [(p0, p1) for p0, p1 in poses if p1[:1].isalpha()]
+                result = [self.lemmatizer.lemmatize(lemma[0], pos=lemma[1][:1].lower()) + '/' + lemma[1] for lemma in posesList]
+
+                logging.info("NLPAnalyzer - doMorphemeAnalysis() - lemmatization :: %s " % result)
